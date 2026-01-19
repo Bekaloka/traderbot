6971dfea41a6b1f3c84e9bbdd3f97a0a69512fd1
Bekaloka <bekaakeb22@gmail.com>
2026-01-19 22:51:03 +0500

    Suppress Flask logging noise in keep_alive.py
    Added logging configuration to suppress Flask noise in logs.

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from threading import Thread
+import logging
+
+# Отключаем лишний шум от Flask в логах
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask('')
 
@@ -8,7 +13,8 @@ def home():
     return "I am alive and trading!"
 
 def run():
-    # Запускаем на порту 8080 или том, который даст Render
+    # Render.com ищет порт 10000 или берет из окружения
+    # Но для внутреннего теста ставим 8080
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
